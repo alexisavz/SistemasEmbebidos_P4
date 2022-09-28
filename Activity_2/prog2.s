e79bcff0c3f2913e5e2f437ba26036e12d2fa164
Alexis Zugasti <alexiszugasti@gmail.com>
2022-09-27 22:32:37 -0500

    Add commas to instructions

--- Activity_2/prog2.s
+++ Activity_2/prog2.s
@@ -15,41 +15,40 @@ main:	push {ip, lr}
 
 @ First Method ---------------------------------------------------------------------
 
-	ldr r0 =y
-	ldr r1 =a
-	ldr r0 [r0]
-	ldr r0 [r1]
-	ldr r0 =string
-	ldr r2 =y
-	ldr r1 [r2]
+	ldr r0, =y
+	ldr r1, =a
+	ldr r0, [r0]
+	ldr r0, [r1]
+	ldr r0, =string
+	ldr r2, =y
+	ldr r1, [r2]
 	bl printf
 
 @----------------------------------------------------------------------------------
 @ Second Method--------------------------------------------------------------------
 
-	ldr r1 =c
-	ldr r0 =y
-	ldr r1 [r1]
-	str r1 [r0]
-	ldr r0 =string
-	ldr r2 =y
-	ldr r1 [r2]
+	ldr r1, =c
+	ldr r0, =y
+	ldr r1, [r1]
+	str r1, [r0]
+	ldr r0, =string
+	ldr r2, =y
+	ldr r1, [r2]
 	bl printf
 
 @-----------------------------------------------------------------------------------
 @ Third Method----------------------------------------------------------------------
 
-	ldr r1 =b
-	ldr r1 [r1]
-	ldr r0 =y
-	str r0 [r1]
-	ldr r0 =string
-	ldr r2 =y
-	ldr r1 [r2]
+	ldr r1, =b
+	ldr r1, [r1]
+	ldr r0, =y
+	str r0, [r1]
+	ldr r0, =string
+	ldr r2, =y
+	ldr r1, [r2]
 	bl printf
 	
 	pop {ip, pc}
-
 
 
 
